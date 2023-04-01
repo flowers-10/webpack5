ca49d5f4f43f30b9a10074db179db50e616bbcc7
flowers-10 <mgaligigi@163.com>
2023-04-01 22:56:13 +0800

    feat: handle devServer

--- webpack.config.js
+++ webpack.config.js
@@ -83,6 +83,12 @@ module.exports = {
       template: path.resolve(__dirname, "public/index.html"),
     })
   ],
+  // 开发服务器
+  devServer: {
+    host: "localhost", // 启动服务器域名
+    port: "3000", // 启动服务器端口号
+    open: true, // 是否自动打开浏览器
+  },
   // 模式
   mode: "development", // 开发模式
 };
