8c738832bc883c1db1a93d3778da7c52b356660e
flowers-10 <mgaligigi@163.com>
2023-04-09 10:10:39 +0800

    test

---
+++ dist/static/js/src_js_math_js.chunk.js
@@ -0,0 +1,21 @@
+"use strict";
+(self["webpackChunkwebpack_code"] = self["webpackChunkwebpack_code"] || []).push([["src_js_math_js"],{
+
+/***/ "./src/js/math.js":
+/*!************************!*\
+  !*** ./src/js/math.js ***!
+  \************************/
+/***/ (function(__unused_webpack_module, __webpack_exports__, __webpack_require__) {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": function() { return /* binding */ count; }
+/* harmony export */ });
+function count(x, y) {
+  return x - y;
+}
+
+/***/ })
+
+}]);
+//# sourceMappingURL=src_js_math_js.chunk.js.map
